d4127ac3ae9a0aba9b879a163132afc2501c13f0
narny29 <nayanjyotikakati1997@gmail.com>
2018-03-06 22:53:49 +0530

    Add files via upload

--- asset1/assets/js/sounds.js
+++ asset1/assets/js/sounds.js
@@ -1,6 +1,6 @@
 var sounds = [	new Audio('/asset1/assets/sounds/Dry-Kick.wav'), 
-				new Audio('/asset1/assets/sounds/Bamboo.WAV'),
-				new Audio('/asset1/assets/sounds/lg_snare.WAV'),  
+				new Audio('/asset1/assets/sounds/Bamboo.wav'),
+				new Audio('/asset1/assets/sounds/lg_snare.wav'),  
 				new Audio('/asset1/assets/sounds/Clap-1.wav')];
 
 var aud_map = [0,1,2,3];
