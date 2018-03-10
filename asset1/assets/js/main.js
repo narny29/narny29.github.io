6b3a85817ee19de281b0823ebf6c9ded0005a7da
narny29 <nayanjyotikakati1997@gmail.com>
2018-03-10 12:08:10 +0530

    Update main.js

--- asset1/assets/js/main.js
+++ asset1/assets/js/main.js
@@ -1,6 +1,6 @@
-const AMP_THRESHOLD = 40; /* Threshold amplitude */
-const POS_SLOPE_THRESHOLD = 25; /* Threshold positive slope */
-const NEG_SLOPE_THRESHOLD = -25; /* Threshold negative slope */
+const AMP_THRESHOLD = 20; /* Threshold amplitude */
+const POS_SLOPE_THRESHOLD = 12; /* Threshold positive slope */
+const NEG_SLOPE_THRESHOLD = -12; /* Threshold negative slope */
 
 var recording = false; /* recording or not */
 var playing = false; /* playing or not */
